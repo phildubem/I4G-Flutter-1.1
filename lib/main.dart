820980ea21337ebefcebd8dc24b153968995d74a
Phil Dubem <phildubem@gmail.com>
2021-05-01 23:21:20 +0100

    Second Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,113 +1,209 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(PortfolioApp());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class PortfolioApp extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
+
+   //var media = MediaQuery.of(context);
+
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor:  Colors.white.withOpacity(0.95),
+        appBar: AppBar(
+          elevation: 3.0,
+          backgroundColor: Color(0xFF73AEF5),
+          title:  Text(
+            'My Portfolio',
+            textDirection: TextDirection.ltr,
+          ),
+          centerTitle: true,
         ),
+
+       body: SingleChildScrollView(
+         child: Column(
+           children: [
+             Material(
+               elevation: 3.0,
+               child: Container(
+                 height: 250,
+                 decoration: BoxDecoration(
+                   gradient: LinearGradient(
+                     begin: Alignment.topCenter,
+                     end: Alignment.bottomCenter,
+                     colors: [
+                       Color(0xFF73AEF5),
+                       Color(0xFF61A4F1),
+                       Color(0xFF478DE0),
+                       Color(0xFF398AE5),
+                     ],
+                     stops: [0.1, 0.4, 0.7, 0.9],
+                   ),
+                 ),
+                 child: Center(
+                   child: Container(
+                     height: 200,
+                     width: 200,
+                     decoration: BoxDecoration(
+                       borderRadius: BorderRadius.circular(100),
+                       border: Border.all(
+                           width: 2.0, color: Colors.white
+                       ),
+                     ),
+                     child: Center(
+                       child: Container(
+                         height: 180,
+                         width: 180,
+                         decoration: BoxDecoration(
+                           image: DecorationImage (
+                             image:  AssetImage('assets/images/avi.png'),
+                               fit: BoxFit.cover
+                           ),
+                             borderRadius: BorderRadius.circular(100),
+                             color: Colors.transparent
+                         ),
+                       ),
+                     ),
+                   ),
+                 ),
+               ),
+             ),
+
+           Padding(
+             padding: const EdgeInsets.all(10.0),
+             child: Column(
+               children: [
+                 Container(
+                   margin: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                   height: 80,
+                   width: double.infinity,
+                   decoration: BoxDecoration(
+                       borderRadius: BorderRadius.circular(10),
+                       color: Colors.white
+                   ),
+                   child: Padding(
+                     padding: const EdgeInsets.all(10),
+                     child: Column(
+                       mainAxisAlignment: MainAxisAlignment.start,
+                       crossAxisAlignment: CrossAxisAlignment.start,
+                       children: [
+                         Row(
+                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                           children: [
+                             Text('Phil Anusionwu',
+                               style: TextStyle(
+                                 fontSize: 20,
+                                 fontWeight: FontWeight.w700,
+                                 color: Color(0xFF398AE5),
+                               ),),
+                             Text('ZUR0016847KO',
+                               style: TextStyle(
+                                 fontSize: 15,
+                                 fontWeight: FontWeight.w500,
+                                 color: Color(0xFF398AE5),
+                               ),),
+                           ],
+                         ),
+                         SizedBox(height: 8),
+                         Text('Web & Mobile Developer',
+                           style: TextStyle(
+                             fontSize: 15,
+                             fontWeight: FontWeight.w500,
+                           ),),
+                       ],
+                     ),
+                   ),
+                 ),
+                 SizedBox(height: 10),
+                 Container(
+                   margin: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                   height: 140,
+                   width: double.infinity,
+                   decoration: BoxDecoration(
+                       borderRadius: BorderRadius.circular(10),
+                       color: Colors.white
+                   ),
+                   child: Padding(
+                     padding: const EdgeInsets.all(10),
+                     child: Column(
+                       mainAxisAlignment: MainAxisAlignment.start,
+                       crossAxisAlignment: CrossAxisAlignment.start,
+                       children: [
+                         Text('I am a developer with experience in web and mobile development. I consider myself jovial and a team player who is always willing to learn.\nMy hobbies include, eating, coding, minding my business and generally having fun.',
+                           style: TextStyle(
+                             fontSize: 15,
+                           ),),
+                       ],
+                     ),
+                   ),
+                 ),
+                 SizedBox(height: 10),
+                 Container(
+                   margin: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                   height: 115,
+                   width: double.infinity,
+                   decoration: BoxDecoration(
+                       borderRadius: BorderRadius.circular(10),
+                       color: Colors.white
+                   ),
+                   child: Padding(
+                     padding: const EdgeInsets.all(10),
+                     child: Column(
+                       mainAxisAlignment: MainAxisAlignment.start,
+                       crossAxisAlignment: CrossAxisAlignment.start,
+                       children: [
+                         Center(
+                           child: Text('Let\'s connect on social media',
+                             style: TextStyle(
+                               fontSize: 15,
+                             ),),
+                         ),
+                         SizedBox(height: 13),
+                         Row(
+                           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                           children: [
+                             Image(
+                               image: AssetImage('assets/images/fb.png'),
+                             ),
+                             Image(
+                               image: AssetImage('assets/images/github.png'),
+                             ),
+                             Image(
+                               image: AssetImage('assets/images/linked.png'),
+                             ),
+                             Image(
+                               image: AssetImage('assets/images/tweet.png'),
+                             ),
+                           ],
+                         ),
+                         SizedBox(height: 10),
+                         Center(
+                           child: Text('@phildubem',
+                             style: TextStyle(
+                               fontSize: 15,
+                               fontWeight: FontWeight.w500,
+
+                             ),),
+                         ),
+                       ],
+                     ),
+                   ),
+                 ),
+               ],
+             ),
+           ),
+
+           ],
+         ),
+
+
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
